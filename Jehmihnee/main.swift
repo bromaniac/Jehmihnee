510715d4bda13f522f5ce08f9f1a1c5a7b466be7
Fredrik Broman <fredrik.broman@me.com>
2021-02-06 00:00:13 +0100

    Initial code commit

--- Jehmihnee/main.swift
+++ Jehmihnee/main.swift
@@ -6,6 +6,118 @@
 //
 
 import Foundation
+import Network
 
-print("Hello, World!")
+@available(macOS 10.14, *)
+class Main {
+
+    init(hostName: String, port: Int) {
+        let host = NWEndpoint.Host(hostName)
+        let port = NWEndpoint.Port("\(port)")!
+        let parameters = Main.getTLSParameters(allowInsecure: true, queue: .main)
+        self.connection = NWConnection(host: host, port: port, using: parameters)
+    }
+
+    let connection: NWConnection
+
+    func start() {
+        NSLog("will start")
+        self.connection.stateUpdateHandler = self.didChange(state:)
+        self.startReceive()
+        self.connection.start(queue: .main)
+    }
+
+    func stop() {
+        self.connection.cancel()
+        NSLog("did stop")
+        exit(0)
+    }
+
+    private func didChange(state: NWConnection.State) {
+        switch state {
+        case .setup:
+            break
+        case .waiting(let error):
+            NSLog("is waiting: %@", "\(error)")
+        case .preparing:
+            break
+        case .ready:
+            break
+        case .failed(let error):
+            NSLog("did fail, error: %@", "\(error)")
+            self.stop()
+        case .cancelled:
+            NSLog("was cancelled")
+            self.stop()
+        @unknown default:
+            break
+        }
+    }
+
+    private func startReceive() {
+        self.connection.receive(minimumIncompleteLength: 1, maximumLength: 65536) { data, _, isDone, error in
+            if let data = data, !data.isEmpty {
+                //NSLog("did receive, data: %@", data as NSData)
+                let str = String(data: data, encoding: .utf8)
+                NSLog("did receive, data: %@", str ?? "foo")
+            }
+            if let error = error {
+                NSLog("did receive, error: %@", "\(error)")
+                self.stop()
+                return
+            }
+            if isDone {
+                NSLog("did receive, EOF")
+                self.stop()
+                return
+            }
+            self.startReceive()
+        }
+    }
+
+    func send(line: String) {
+        let data = Data("\(line)\r\n".utf8)
+        self.connection.send(content: data, completion: NWConnection.SendCompletion.contentProcessed { error in
+            if let error = error {
+                NSLog("did send, error: %@", "\(error)")
+                self.stop()
+            } else {
+                NSLog("did send, data: %@", data as NSData)
+            }
+        })
+    }
+
+    static private func getTLSParameters(allowInsecure: Bool, queue: DispatchQueue) -> NWParameters {
+        let options = NWProtocolTLS.Options()
+
+        sec_protocol_options_set_verify_block(options.securityProtocolOptions, { (sec_protocol_metadata, sec_trust, sec_protocol_verify_complete) in
+            
+            let trust = sec_trust_copy_ref(sec_trust).takeRetainedValue()
+            
+            var error: CFError?
+            if SecTrustEvaluateWithError(trust, &error) {
+                sec_protocol_verify_complete(true)
+            } else {
+                if allowInsecure == true {
+                    sec_protocol_verify_complete(true)
+                } else {
+                    sec_protocol_verify_complete(false)
+                }
+            }
+            
+        }, queue)
+        
+        return NWParameters(tls: options)
+    }
+
+    static func run() -> Never {
+        let m = Main(hostName: "gemini.circumlunar.space", port: 1965)
+        m.start()
+
+        m.send(line: "gemini://gemini.circumlunar.space/")
+        dispatchMain()
+    }
+}
+
+Main.run()
 
